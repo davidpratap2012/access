3e13af4d12a5d614c78f24f3a16aa6983f0a3241
Pratap David <davidpratap2012@live.com>
2021-01-25 13:38:25 +0530

    trying a new config of tokens

--- scripts/generate.py
+++ scripts/generate.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
     # Load best model
     best_model_dir = prepare_models()
     recommended_preprocessors_kwargs = {
-        'LengthRatioPreprocessor': {'target_ratio': 1.0},
-        'LevenshteinPreprocessor': {'target_ratio': 0.25},
-        'WordRankRatioPreprocessor': {'target_ratio': 0.25},
+        'LengthRatioPreprocessor': {'target_ratio': 1.05},
+        'LevenshteinPreprocessor': {'target_ratio': 0.5},
+        'WordRankRatioPreprocessor': {'target_ratio': 0.5},
         'SentencePiecePreprocessor': {'vocab_size': 10000},
     }
     preprocessors = get_preprocessors(recommended_preprocessors_kwargs)
